8fe31e518ebdebf8edafc7a1f66b83659a77fa9c
Michael Prather <mikeprather20@gmail.com>
2022-08-07 11:51:40 -0500

    adding routes

--- app/shop/routes.py
+++ app/shop/routes.py
@@ -6,3 +6,32 @@ from app.models import User, Product, shop, db
 
 shop = Blueprint('shop', __name__, template_folder='shoptemplates')
 
+#1 Route for available product (shopfront)
+
+@shop.route('/login', methods = ["GET","POST"])
+def shopfront():
+    pass
+
+#2 Route for single product with product info (clicked item in shopfront)
+
+@shop.route('/login', methods = ["GET","POST"])
+def productinfo():
+    pass
+#3 Route to add product to cart with @login_required
+
+@shop.route('/login', methods = ["GET","POST"])
+@login_required
+def add2cart():
+    pass
+#4 Route to show all items added to cart and the total price
+
+@shop.route('/login', methods = ["GET","POST"])
+@login_required
+def showcart():
+    pass
+#5 Route to remove all items from cart
+
+@shop.route('/login', methods = ["GET","POST"])
+@login_required
+def removeall():
+    pass
